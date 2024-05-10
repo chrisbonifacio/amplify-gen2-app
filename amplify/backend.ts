26a9770429f05984d4bd5f49e35d58cc792a8f0b
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-05-10 01:31:39 -0400

    update main

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -6,29 +6,3 @@ const backend = defineBackend({
   auth,
   data,
 });
-
-const cfnUserPool = backend.auth.resources.cfnResources.cfnUserPool;
-
-// override values
-
-cfnUserPool.adminCreateUserConfig = undefined;
-
-cfnUserPool.policies = {
-  passwordPolicy: {
-    minimumLength: 8,
-    requireLowercase: false,
-    requireNumbers: false,
-    requireSymbols: false,
-    requireUppercase: false,
-    temporaryPasswordValidityDays: undefined,
-    typeHint: undefined,
-  },
-};
-
-cfnUserPool.verificationMessageTemplate = undefined;
-
-cfnUserPool.accountRecoverySetting = undefined;
-
-cfnUserPool.mfaConfiguration = "OFF";
-
-cfnUserPool.smsVerificationMessage = undefined;

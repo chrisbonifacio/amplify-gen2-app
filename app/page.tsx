bd8ebd1171f643ac7d27a3760bfd46416facc6bc
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-05-17 12:35:45 -0400

    update batch get item sample

--- app/page.tsx
+++ app/page.tsx
@@ -2,60 +2,13 @@
 import { useEffect, useState } from "react";
 import { Schema } from "@/amplify/data/resource";
 import { generateClient } from "aws-amplify/api";
-import {
-  WithAuthenticatorProps,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
+import { WithAuthenticatorProps } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import { getCurrentUser } from "aws-amplify/auth";
 
 const client = generateClient<Schema>();
 
 function Home({ signOut }: WithAuthenticatorProps) {
   const [phoneNumbers, setPhoneNumbers] = useState<any>();
-
-  // const test = async () => {
-  //   const currentUser = await getCurrentUser();
-
-  //   const { data: recieveUnreadMessages } =
-  //     await client.models.Message.listByReceiverIdAndViewedTimeStamp({
-  //       receiverId: currentUser.userId,
-  //       viewedTimeStamp: {
-  //         attributeExists: false,
-  //       },
-  //     });
-
-  //   const { data: sentUnreadMessages } =
-  //     await client.models.Message.listBySenderIdAndViewedTimeStamp({
-  //       senderId: currentUser.userId,
-  //       viewedTimeStamp: {
-  //         attributeExists: true,
-  //       },
-  //     });
-
-  //   console.log({ recieveUnreadMessages, sentUnreadMessages });
-  // };
-
-  const createLocation = async () => {
-    const { data, errors } =
-      await client.mutations.createNewLocationWithLongLat({
-        id: "12345",
-        address: "2230 University Ave",
-        name: "Home",
-        lat: 40.859468005470646,
-        long: -73.90785841528128,
-      });
-  };
-
-  const listLocations = async () => {
-    const { data, errors } = await client.queries.listEventsWithDecodedLatLong({
-      authMode: "apiKey",
-    });
-
-    console.log(data);
-
-    // if (data) setLocations(data);
-  };
 
   const checkActivePhoneNumbers = async () => {
     const largeArray = new Array(95).fill("");
@@ -79,8 +32,7 @@ function Home({ signOut }: WithAuthenticatorProps) {
       <button onClick={signOut}>Sign Out</button>
       {/* <button onClick={test}>Send Message</button> */}
       <button onClick={checkActivePhoneNumbers}>Check Phone Numbers</button>
-      <button onClick={createLocation}>Create Location</button>
-      <button onClick={listLocations}>List Locations</button>
+
       <div>
         <pre>{JSON.stringify(phoneNumbers, null, 2)}</pre>
       </div>

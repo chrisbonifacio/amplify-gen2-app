bb995c6a3a7c4ba16a9b35e765a05d89f50145c8
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-06-10 12:37:48 -0400

    update amplify packages

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,9 +3,44 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 const schema = a.schema({
   Todo: a
     .model({
-      content: a.string(),
+      title: a.string().required(),
+      goalId: a.id(),
+      goal: a.belongsTo("Goal", "goalId"),
     })
-    .authorization((allow) => [allow.owner(), allow.publicApiKey()]),
+    .authorization((allow) => [
+      allow.publicApiKey(),
+      // allow.owner(),
+      allow.group("Users").to(["read"]),
+      allow.group("Administrators"),
+    ]),
+  Goal: a
+    .model({
+      title: a.string().required(),
+      todos: a.hasMany("Todo", "goalId"),
+    })
+    .authorization((allow) => [
+      allow.owner(),
+      allow.group("Users").to(["read"]),
+      allow.group("Administrators"),
+    ]),
+  Message: a
+    .model({
+      userId: a.id().required(),
+      chatId: a.id().required(),
+      chat: a.belongsTo("Chat", "chatId"),
+      content: a.string(),
+      time: a.timestamp(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+
+  Chat: a
+    .model({
+      ownerId: a.id().required(),
+      chatName: a.string().required(),
+      messages: a.hasMany("Message", "chatId"),
+      time: a.timestamp(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;

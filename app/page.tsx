86dd67d5ea5f90970c5c3489573a7b2a9b323df6
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-06-18 12:17:02 -0400

    update single table design doc

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   };
 
   const getBestPerformancebyClassID = async () => {
-    const { data, errors } = await client.queries.getBestPerformanceByClassIc({
+    const { data, errors } = await client.queries.getBestPerformanceByClassId({
       classId: "<classID>",
     });
 
@@ -56,50 +56,10 @@ export default function Home() {
     console.log(data);
   };
 
-  // const listUsersBySearchTermFunction = async () => {
-  //   try {
-  //     const { data, errors } = await client.models.User.listUsersBySearchTerm({
-  //       searchTerm: "ABCD",
-  //       firstNameLastName: {
-  //         eq: {
-  //           firstName: "Chris",
-  //           lastName: "Bonifacio",
-  //         },
-  //       },
-  //     });
-
-  //     if (errors) {
-  //       console.log({ errors });
-  //     }
-
-  //     const res = await client.graphql({
-  //       query: listUsersBySearchTerm,
-  //       variables: {
-  //         searchTerm: "ABCD",
-  //         firstNameLastName: {
-  //           eq: {
-  //             firstName: "Chris",
-  //             lastName: "Bonifacio",
-  //           },
-  //         },
-  //       },
-  //     });
-
-  //     console.log(data);
-
-  //     console.log(res);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <button onClick={addRacerToRace}>Add Racer to Race</button>
-        {/* <button onClick={listUsersBySearchTermFunction}>
-          List Users By Search Term
-        </button> */}
       </div>
     </main>
   );
